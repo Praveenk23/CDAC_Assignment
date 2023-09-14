55f2cd1d602f68ed60390c34a8f419ea68a2ee63
Praveenk23 <95569712+Praveenk23@users.noreply.github.com>
2023-09-14 13:24:20 +0530

    Update 13.c

--- Programming_Concept/13.c
+++ Programming_Concept/13.c
@@ -5,7 +5,11 @@ int main(){
 	int flag = 0;
 	printf("Enter a number: ");
 	scanf("%d", &n);
-	for(int i = 2; i * i < n; i++){
+	if(n <= 1){
+		printf("Not a prime number\n");
+		return 0;
+	}
+	for(int i = 2; i <= n / 2; i++){
 		if(n % i == 0){
 			flag = 1;
 			break;
